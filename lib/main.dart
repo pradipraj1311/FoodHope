631c9486b0d5cf9e96ce191529b5c1b1943ffbb7
pradipraj1311 <pradiprajwadi0013@gmail.com>
2026-03-25 16:20:32 +0530

    configured Firebase project and initialized in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'screens/role_selection_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async {
+  // Required to ensure Flutter framework is ready before Firebase initializes
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
-  runApp(FoodHopeApp());
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const FoodHopeApp());
 }
 
 class FoodHopeApp extends StatelessWidget {
+  const FoodHopeApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
